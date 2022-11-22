aa69e75f446404b5f2c7d344d7fa391d31507be9
SyukriHaziq <97055134+SyukriHaziq@users.noreply.github.com>
2022-11-22 16:01:03 +0800

    Add files via upload

---
+++ ShapeV3/Shape/Shape/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle aRec = new Rectangle();
+
+            Console.WriteLine("Width = {0}, Height = {1}", aRec.Width, aRec.Height);
+            aRec.Width = 4;
+            aRec.Height = 5;
+            Console.WriteLine("Width = {0}, Height = {1}", aRec.Width, aRec.Height);
+
+            Console.ReadKey();
+
+            Square aSq = new Square(8);
+
+            Console.WriteLine("Length = {0}", aSq.len);
+            Console.WriteLine("Area = {0}, Circumference = {1}", aSq.Area(), aSq.Circumference());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ShapeV3/Shape/Shape/Rectangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    class Rectangle : IShape
+    {
+        private double _width;
+        private double _height;
+
+        public double Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        public double Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public Rectangle()
+        {
+            _width = 0;
+            _height = 0;
+        }
+
+        public Rectangle(double Width, double Height)
+        {
+            _width = Width;
+            _height = Height;
+        }
+
+        public double Area()
+        {
+            return _width * _height;
+        }
+
+        public double Circumference()
+        {
+            return 2 * (_width + _height);
+        }
+    }
+}
